c6d6952a6d21c3522cfb6762538bd63bee2f15a9
kofiasafo-boakye <kofi.asafo-boakye@ashesi.edu.gh>
2023-06-26 22:36:47 +0000

    updates

--- client/src/pages/editProfile.js
+++ client/src/pages/editProfile.js
@@ -2,6 +2,19 @@ import { useParams, useNavigate } from "react-router-dom";
 import {BACKEND_API} from "../api/backend_api"
 import useFetch from "../hooks/useFetch";
 import { useState, useEffect } from "react";
+import Axios from "axios";
+
+
+
+
+const initialState = {
+    firstName : "",
+    lastName: "",
+    username: "",
+    email: "",
+    profilePicture: ""
+}
+
 
 const EditProfile = () => {
     const {id} = useParams()
@@ -9,8 +22,21 @@ const EditProfile = () => {
     const navigate = useNavigate()
     const [data, setData] = useState([]); 
 
-    const {data: user, isPending, error} = useFetch(`${BACKEND_API}/user/profile/${id}`)
+    // const {data: user, isPending, error} = useFetch(`${BACKEND_API}/user/profile/${id}`)
 
+    const [state, setState] = useState(initialState)
+    const {firstName, lastName, username, email, profilePicture} = state
+
+    const url = `${BACKEND_API}/user/profile/${id}`
+
+    useEffect(() => {
+        Axios.get(url)
+        .then((response) => {
+            setState({...response.data})
+            // console.log(response.data[0])
+            console.log(profilePicture)
+        })
+    }, [url])
 
     return ( 
         <form action="">
